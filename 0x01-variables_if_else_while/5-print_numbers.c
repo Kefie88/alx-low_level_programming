839fe1c81a74f61cd10369cda13104d935f54e85
Kefyalew <you@example.com>
2022-06-03 07:52:43 -0700

    My commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,12 @@
  * description - prints all single digit numbers of base 10
  * Return: always 0
  */
-int main()
+int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 		printf("%d", i);
+	putchar('\n');
 	return (0);
 }
