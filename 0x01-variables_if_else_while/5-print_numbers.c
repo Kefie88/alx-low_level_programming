65df7e27c051252657f6d1000e3d3428f06a4eb8
Kefyalew <you@example.com>
2022-06-03 07:48:05 -0700

    My commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description - prints all single digit numbers of base 10
+ * Return: always 0
+ */
+int main()
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", i)
+	return (0);
+}
