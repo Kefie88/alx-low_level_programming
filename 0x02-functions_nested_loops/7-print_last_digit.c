a7aeb8b52b0be8cb476036ffa9e1bc44b05d50b9
Kefyalew <you@example.com>
2022-06-07 11:17:43 +0000

    My commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,17 @@
 #include "main.h"
 /**
- *print_last_digit - prints the last digit
+ *print_last_digit - prints the last digit of a number
  *@num: the number where its last digit to be computed
  *Return: return the number modulo 10
  */
 int print_last_digit(int num)
 {
-	return (num % 10);
+	int n;
+
+	if (num < 0)
+		n = (num % 10) * -1;
+	else
+		n = num % 10;
+	_putchar(n + '0');
+	return (n);
 }
