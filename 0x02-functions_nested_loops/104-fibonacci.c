cc4145192bd7853c1d5ab52e8c7e09e9fc7ba289
Kefyalew <you@example.com>
2022-06-07 14:09:36 +0000

    My commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Description - finds and prints first 98 fibonacci numbers
+ * Return: returns 0
+ */
+int main(void)
+{
+	long int f1 = 1, f2 = 2, next = f1 + f2;
+	int i = 1;
+
+	printf("%ld, ", f1);
+	printf("%ld, ", f2);
+	while (i < 97)
+	{
+		if (i == 96)
+		{
+			printf("%ld", next);
+		}
+		else
+		{
+			printf("%ld, ", next);
+			next = f1 + f2;
+			f1 = f2;
+			f2 = next;
+		}
+		i++;
+	}
+	return (0);
+}
