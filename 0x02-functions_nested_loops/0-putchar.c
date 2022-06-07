ba62ffb49ad0265e44df13c05827f5555b7eed0b
Kefyalew <you@example.com>
2022-06-07 09:33:07 +0000

    My commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - entry point
  * Description - prints _putchar
@@ -6,7 +6,14 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
+	int i = 0;
+	char ch [] = "_putchar";
+
+	while (i < 8)
+	{
+		_putchar(ch [i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
