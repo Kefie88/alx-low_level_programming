de52d5de2967f0c4e3d1cab45d7738cf874636a8
Kefyalew <you@example.com>
2022-06-07 12:13:24 +0000

    My commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the starting of the natural number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar(i + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+}
