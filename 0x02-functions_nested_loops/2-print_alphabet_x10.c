c7f1336d7864479868ce1a3fa489d10fd7551b4f
Kefyalew <you@example.com>
2022-06-07 09:39:23 +0000

    My commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include "main"
+#include "main.h"
 /**
- * main - 
- * Description -
- * REturn: return 0
+ * print_alphabet_x10 - print alphabet 10 times in lowercase
  */
 void print_alphabet_X10(void)
 {
 	int i = 10;
-	char ch;
-	
-	for (ch = 'a'; ch <= 'z'; ch++)
+
+	while (i < 10)
 	{
-		_putchar(ch);
+		char ch;
+	
+		for (ch = 'a'; ch <= 'z'; ch++i)
+		{
+			_putchar(ch);
+		}
 	}
 	_putchar('\n')
 	
