4814dae8e7d94817d743dc39c5646b1a40035b54
Kefyalew <you@example.com>
2022-06-07 13:28:19 +0000

    My commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-	int f1 = 0, f2 = 1, next = 0;
+	int f1 = 0, f2 = 1, next = 0, n = 1;
 
 	next = f1 + f2;
-	while (next <= 50)
+	while (n<= 50)
 	{
 		printf("%d, ", next);
 		f1 = f2;
 		f2 = next;
 		next = f1 + f2;
+		n++;
 	}
 	return (0);
 }
