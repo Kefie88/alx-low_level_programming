3e97a2a0fbd0e8143edbe0244be4f662b7e23dcc
Kefyalew <you@example.com>
2022-06-08 13:31:24 +0000

    My commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,15 +4,25 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, n;
 
 	for (i = 0; i < 10; i++)
 	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
 		for (j = 1; j < 10; j++)
 		{
-			_putchar((i * j) + '0');
-			_putchar(',');
-			_putchar(' ');
+			n = (i * j);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
 			if (j < 9)
 			{
 				_putchar(',');
