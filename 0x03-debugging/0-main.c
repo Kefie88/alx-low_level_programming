b5628eb97a95c857a9b55c07e17fde12dd237d11
Kefyalew <you@example.com>
2022-06-08 13:16:35 +0000

    My commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
- * main - test function for +ve or -ve
+ * main - Test function for +ve or -ve
  * Return: returns 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 return (0);
