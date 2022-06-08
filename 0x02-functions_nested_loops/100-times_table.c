4e9fbec35c368f80ab48b8a8731b28bbe4eb131c
Kefyalew <you@example.com>
2022-06-08 13:51:10 +0000

    My late commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, t;
 
 	if (n >= 0 && n <= 15)
 	{
@@ -14,9 +14,34 @@ void print_times_table(int n)
 		{
 			for (j = 0; j < n; j++)
 			{
-				printf("%d,  ", i * j);
+				t = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (t < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((t % 10) + '0');
+				}
+				else if (t >= 10 && t < 100)
+				{
+					_putchar(' ');
+					_putchar((t / 10) + '0');
+					_putchar((t % 10) + '0');
+				}
+				else if (t >= 100 && j != 0)
+				{
+					_putchar((t / 100) + '0');
+					_putchar((t / 10) + '0');
+					_putchar((t % 10) + '0');
+				}
+				else
+					_putchar ((t % 10) + '0');
 			}
-			printf("%d\n", i * j);
+			_putchar('\n');
 		}
 	}
 }
