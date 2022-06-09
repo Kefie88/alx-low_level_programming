b7838c71bd3dcead7d8989ecd2911146d9794dd9
Kefyalew <you@example.com>
2022-06-09 15:01:42 +0000

    More function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	i = j = 0;
 	while (i < 10)
 	{
-		while (j <= 14)
+		while (j < 15)
 		{
 			if (j >= 10)
 				_putchar(j / 10 + '0');
-			_putchar(j + '0');
-			++j;
+			_putchar(j % 10 + '0');
+			j++;
 		}
 		_putchar('\n');
 		j = 0;
