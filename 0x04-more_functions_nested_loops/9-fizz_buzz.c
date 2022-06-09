8510b88570b0dd6d446900ecf46b1fdfd50e3478
Kefyalew <you@example.com>
2022-06-09 15:43:57 +0000

    More function

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description - prints Fizz and Buzz for multiples of 3 and 5 respectively and
+ * prints FizzBuzz for multiples of both
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
