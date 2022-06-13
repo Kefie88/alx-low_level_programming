eed91c0530c8ce4c003ebbd78880bbf3c5f61c64
Kefyalew <you@example.com>
2022-06-13 12:53:04 +0000

    Pointer

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_array - prints n elements of an array of integers, followed by new line
+ * print_array - prints n elements of an array of integers,
+ * followed by new line
  * @a: firest paramenter
  * @n: second parameter
  * Return: nothing
