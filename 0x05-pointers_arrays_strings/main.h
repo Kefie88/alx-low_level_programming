cfc9f0d99ad90e4d9fe893ac2c6482a88ce1ee1f
Kefyalew <you@example.com>
2022-06-13 11:27:32 +0000

    Pointer

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+int _putchar(char);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+vod _puts(char *str);
+ivoid print_rev(char *s);
+void puts2(char *str);
+void rev_string(char *s);
+int string_length(char*);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+#endif
