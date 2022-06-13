710759b3611d6a83bdee166ac3bd94308c4d5134
Kefyalew <you@example.com>
2022-06-13 13:00:17 +0000

    Pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - string coppy
+ * @dest: first parameter (destination)
+ * @src: second parameter (source)
+ * Return: returns pointer to destination, dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = *(src + i);
+}
