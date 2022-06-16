e45d1cbb56a9b28cffd7b89ff552e23715478e93
Kefyalew <you@example.com>
2022-06-16 05:42:12 +0000

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i = 0, len = 0;
 
 	while (src[i] && i < n)
 	{
 		*(dest + i) = src[i];
 		i++;
+		len++;
 	}
+	for (i = len; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
