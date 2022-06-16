e98074f72ad868d5c9b96d7413766b5597abbd3a
Kefyalew <you@example.com>
2022-06-16 05:21:29 +0000

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0, len1, len2;
 
-	while (dest[i])
+	while (dest[i++])
 		len1++;
-	while (src[j])
+	while (src[j++])
 		len2++;
-	if ( len2 >= n)
+	if (len2 >= n)
 	{
 		for (i = 0; i < n; i++, len1++)
 			*(dest + len1) = *(src + i);
