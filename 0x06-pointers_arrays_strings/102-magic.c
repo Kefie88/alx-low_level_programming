848e42ad866b18176e007a2ab93f2637f93bf664
Kefyalew <you@example.com>
2022-06-16 06:57:01 +0000

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,11 +14,11 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
-	 * Remember
+	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
+	 * - you are not allowed to code anything else thatn this line of code
 	 */
 	*(p + 5) = 98;
 	printf("a[2] = %d\n", a[2];
