07cdccb41a227da38273944370f1a3c3e1e8d84b
Kefyalew <you@example.com>
2022-06-22 13:08:31 +0000

    Recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * is_palindrome - check whether a string is palindrome
+ * @s: given string
+ * Return: 1 if a string is palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * wildcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: Returns 1 if the strings can be considered identical,
+ * 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	
+}
