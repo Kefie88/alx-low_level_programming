a08cdc178ce68c0b61d68431415ad7a8b52fd457
Kefyalew <you@example.com>
2022-06-24 12:01:36 +0000

    Argc and Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: counter
+ * @argv: pointer
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
