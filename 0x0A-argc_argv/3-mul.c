076bbb8fb158a1fe5dd7b6a10d24fb57251dcbe4
Kefyalew <you@example.com>
2022-06-24 12:22:37 +0000

    Argc and Argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: counter
+ * @argv: pointer
+ * Return: returns 1 if program does not receive two arguments,
+ * 0 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int i, multip = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 0; i < argc;  i++)
+	{
+		multip *= atoi(argv[i]);
+	}
+	printf("%d\n", multip);
+	return (0);
+}
