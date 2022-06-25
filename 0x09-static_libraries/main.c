8418d446c57567c2983291f171a37d53847af08e
Kefyalew <you@example.com>
2022-06-25 10:03:11 +0000

    static Library

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - manin entry of cod
+ *Return: 0 always succesful
+ */
+
+int main(void)
+{
+_puts("\"At the end of the day, my goal was to be \"\n\t- Kevin Mitnick");
+return (0);
+}
+
