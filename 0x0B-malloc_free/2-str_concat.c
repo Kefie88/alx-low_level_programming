b5b23c969c31347d95084c830c7d3d37ddc65674
Kefyalew <you@example.com>
2022-06-28 12:17:11 +0000

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,6 +25,6 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i]; i++)
 		Concat_String[ic++] = s1[i];
 	for (i = 0; s2[i]; i++)
-		Concat_string[ic++] = s2[i];
+		Concat_String[ic++] = s2[i];
 	return (Concat_String);
 }
