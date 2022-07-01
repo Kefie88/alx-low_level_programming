b24e8a47139e41d80b80f447024d70059171b299
Kefyalew <you@example.com>
2022-07-01 07:34:33 +0000

    Late More Malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of the memory
+ * Return: pointer to the allocated pointer
+ * if malloc fails, the function should cause normal processor
+ * termination with a status value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
 
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
