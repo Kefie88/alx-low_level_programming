4661102054e44970439ae6189e62f249201233dd
Kefyalew <you@example.com>
2022-07-04 15:26:02 +0000

    Structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,19 @@
-#include <stdlib.h>
-#include "main.h"
+#ifndef dog_h
+#define dog_h
 /**
- * init_dog - initialize a dog structure
- * @d: a dog structure
- * @name: name of dog
- * @age: age of dog
- * @owner: the owner of the dog
- * Return: nothing (void)
+ * struct dog - structure for dog variables
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of owner
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+typedef struct dog
 {
-	if (d != NULL)
-	{
-		d - >name = name;
-		d - >age = age;
-		d - >owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void int_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
