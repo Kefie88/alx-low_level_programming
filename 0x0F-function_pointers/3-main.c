162383976c5b0966cf5fea4dfc76c5267234b7c1
Kefyalew <you@example.com>
2022-07-06 14:27:40 +0000

    Function Pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+/**
+ * main - calculates the result of two integers based on the operand
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	register int num1, num2;
+	int (*funptr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Erro\n");
+		exit(98);
+	}
+	funptr = get_opt_func(argv[2]);
+	if (!funptr)
+	{
+		print("Error\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	printf("%d\n", funptr(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: summation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction
+ * @a: first integer
+ * @b: second integer
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: first int
+ * @b: second int
+ * Return: dividend
+ */
+int op_div(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - modulus
+ * @a: first int
+ * @b: second int
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
