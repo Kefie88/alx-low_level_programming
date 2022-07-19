a3f2d1d7574d3e057288087a737b499f2d893934
Kefyalew <you@example.com>
2022-07-19 07:56:32 +0000

    More Single Linked Lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	if (indext == 0)
+	if (index == 0)
 	{
 		next = (*head)->next;
 		free(*head);
