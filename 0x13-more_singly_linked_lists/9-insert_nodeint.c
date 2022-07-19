705d6f7d64f57128d30788fca361dc7bdbea1fe4
Kefyalew <you@example.com>
2022-07-19 07:36:21 +0000

    More single Linked Lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,34 +9,30 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current;
-	listint_t *new;
+	listint_t *new_node, *tmp;
+	unsigned int i = 0;
 
-	if (head == NULL)
-		return (0);
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (0);
-	new->next = NULL;
-	new->n = n;
+	if (*head == NULL && idx != 0)
+		return (NULL);
+	if (idx != 0)
+	{
+		tmp = *head;
+		for (; i < idx - 1 && tmp != NULL; i++)
+			tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
 	if (idx == 0)
 	{
-		new->next = *head;
-		(*head) = new;
-		return (new);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
-	current = *head;
-	while (idx != 1)
-	{
-		current = current->next;
-		--idx;
-		if (current == NULL)
-		{
-			free(new);
-			return (NULL);
-		}
-	}
-	new->next = current->next;
-	current->next = new;
-	return (new);
+	new_node->next = tmp->next;
+	tmp->next = new_node;
+	return (new_node);
 }
