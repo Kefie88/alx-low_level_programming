65deacd69950fcd591813d7e9b7ca812afb9c507
Kefyalew <you@example.com>
2022-11-09 11:28:04 +0000

    Make File

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,7 +21,7 @@ def num_water_neighbors(grid, i, j):
 
 def island_perimeter(grid):
     """Returns the perimeter of the island in grid."""
-    
+
     perim = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
